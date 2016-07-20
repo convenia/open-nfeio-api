493f6c9c1c8f44a1171ac2784360d209eae840f2
Victor <euventura@gmail.com>
2016-07-20 20:17:07 -0300

    apply psr2

--- tests/openNfeio.php
+++ tests/openNfeio.php
@@ -7,18 +7,17 @@ use Convenia\OpenNfeio\OpenNfeio;
 class tests_openNfeio extends PHPUnit_Framework_TestCase
 {
 
-	public function test_ApiCallTest()
-	{
-		$obj = new OpenNfeio('api_key');
-		$responseArray = $obj->addresses('04110001');
-		$this->assertArrayHasKey('postalCode', $responseArray->get());
-	}
+    public function test_ApiCallTest()
+    {
+        $obj = new OpenNfeio('api_key');
+        $responseArray = $obj->addresses('04110001');
+        $this->assertArrayHasKey('postalCode', $responseArray->get());
+    }
 
-	public function test_exceptionTest()
-	{
-		$this->setExpectedException('Exception');
-		$obj = new OpenNfeio('api_key');
-		$responseArray = $obj->InvalidCall('04110001');
-	}
-
+    public function test_exceptionTest()
+    {
+        $this->setExpectedException('Exception');
+        $obj = new OpenNfeio('api_key,');
+        $responseArray = $obj->InvalidCall('04110001');
+    }
 }
